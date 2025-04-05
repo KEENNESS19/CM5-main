ddb230ffeddde0a7ee964be29c42ba3a943035a3
KEENNESS19 <147995398+KEENNESS19@users.noreply.github.com>
2025-04-05 16:02:26 +0800

    Update color.py

--- demos/color.py
+++ demos/color.py
@@ -12,11 +12,11 @@ blue = (0, 0, 255)
 yellow = (255, 255, 0)
 
 
-color_lower = np.array([0, 43, 46])
-color_upper = np.array([10, 255, 255])
+color_lower = [np.array([0, 160, 100]), np.array([160, 160, 100])]  
+color_upper = [np.array([10, 255, 255]), np.array([179, 255, 255])]
 mode = 1
 
-
+# Skin detection parameters
 skin_lower = np.array([0, 133, 77])  
 skin_upper = np.array([255, 173, 127])
 
@@ -32,11 +32,17 @@ picam2.start()
 def change_color():
     global color_lower, color_upper, mode
     
+    
     color_ranges = [
-        (np.array([0, 160, 100]), np.array([10, 255, 255])),   # red
-        (np.array([40, 60, 75]), np.array([77, 255, 255])),  # green
-        (np.array([100, 150, 100]), np.array([120, 255, 255])),  # blue
-        (np.array([26, 43, 46]), np.array([34, 255, 255]))  # yellow
+      
+        ([np.array([0, 160, 100]), np.array([160, 160, 100])], 
+         [np.array([10, 255, 255]), np.array([179, 255, 255])]),
+        
+        ([np.array([40, 60, 75])], [np.array([77, 255, 255])]),
+      
+        ([np.array([100, 150, 100])], [np.array([120, 255, 255])]),
+    
+        ([np.array([28, 60, 46])], [np.array([34, 255, 255])])
     ]
     
     mode = (mode % 4) + 1
@@ -51,24 +57,28 @@ color_radius = 0
 
 while True:
     frame = picam2.capture_array() 
-   # frame = cv2.convertScaleAbs(frame, alpha=1.2, beta=0)
+   
+    frame = cv2.flip(frame, 1)
+        
     frame_ = cv2.GaussianBlur(frame, (5, 5), 0)
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, color_lower, color_upper)
     
-
+    
+    if mode == 1: 
+        mask1 = cv2.inRange(hsv, color_lower[0], color_upper[0])
+        mask2 = cv2.inRange(hsv, color_lower[1], color_upper[1])
+        mask = cv2.bitwise_or(mask1, mask2)
+    else: 
+        mask = cv2.inRange(hsv, color_lower[0], color_upper[0])
+    
+    # Apply skin color filtering
     ycbcr = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
     skin_mask = cv2.inRange(ycbcr, skin_lower, skin_upper)
-    
-
     mask = cv2.bitwise_and(mask, cv2.bitwise_not(skin_mask))
     
-
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
     
-
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     
     if len(cnts) > 0:
@@ -84,14 +94,14 @@ while True:
             
             dog.attitude(['y', 'p'], [-value_x / 15, value_y / 15])
 
-
+    # Display position and FPS
     cv2.putText(frame, "X:%d, Y:%d" % (int(color_x), int(color_y)), (40, 40), font, 0.8, (0, 255, 255), 3)
     
     fps += 1
     mfps = fps / (time.time() - t_start)
     cv2.putText(frame, "FPS " + str(int(mfps)), (40, 80), font, 0.8, (0, 255, 255), 3)
 
-
+    # Convert and display image
     b, g, r = cv2.split(frame)
     img = cv2.merge((r, g, b))
     colors = [red, green, blue, yellow]
